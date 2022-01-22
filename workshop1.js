1dc617afc43d277d7a974160d4305253426c856a
Jerry Velazquez <Jvr572@gmail.com>
2022-01-22 17:58:35 -0500

    testing cdn

---
+++ workshop1.js
@@ -0,0 +1,8 @@
+// you can create a UI for the tests 
+const test = document.createElement('h1');
+test.innerHTML = 'Workshop 1';
+
+document.body.appendChild(test);
+// the first option was to create assertion test for the UI 
+
+
