c44fdbb20459007ebf751f4ac6b9dd2b463bf821
Jerry Velazquez <Jvr572@gmail.com>
2022-01-23 17:23:39 -0500

    test for tasks 2 & 3;

--- src/workshop1.js
+++ src/workshop1.js
@@ -10,7 +10,12 @@ const html = document.documentElement,
   head = document.querySelector("head"),
   meta = document.querySelector('head meta[charset="UTF-8"]'),
   title = document.querySelector("head title"),
-  sectionCount = document.querySelectorAll("body section").length;
+  sectionCount = document.querySelectorAll("body section").length,
+  img = document.querySelector("img")
+  ScndHeaders = document.querySelectorAll("section h2")
+  ulLstCount = document.querySelectorAll("section ul").length,
+  ulLst = document.querySelectorAll("section ul"),
+  audio = document.querySelector("audio");
 
 
 //doctype must be present 
@@ -26,7 +31,7 @@ console.assert(html.lang.includes("en"), "You must declare a lang attribute equa
 console.assert(meta, "You must declare a meta charset attribute equal to \"UTF-8\" on your html element");
 
 // head tile must be present with value "Profile"
-console.assert(title.innerText.includes("Profile"), "You must declare a title attribute equal to \"Profile - [your name here]\"  on your head element");
+console.assert(title.innerText.includes("Profile"), "You must have a title element in the head tag; the content should contain \"Profile\" - [your name here] ");
 // should have a h1 element with text "Profile"
 console.assert(
 	body.firstElementChild.tagName.toLowerCase() === "h1",
@@ -35,10 +40,28 @@ console.assert(
 
 // must have 2 or more section elements
 console.assert(sectionCount >= 2, "You must have more than 2 section elements in your body");
-// should be a section element ?
-console.log(body.lastElementChild.previousElementSibling);
+
+console.assert(img, "must have an img element in your body");
+console.assert(!!img.getAttribute("alt"), "img element must have an alt attribute, with description of the image");
 
 
+console.assert([...ScndHeaders].some(el => el.innerText.includes("About me")), "You must have a h2 element with text \"About me\"");
+
+console.assert([...ScndHeaders].some(el => el.innerText.includes("Interests")), "You must have a h2 element with text \"Interests\"");
+
+console.assert(ulLstCount >= 2, "You must have at least two ul elements in your body");
+
+console.assert([...ulLst].every(el => el.children.length >= 2), "You must have at least two li elements in each ul element");
+
+console.assert(audio, "You must have an audio element in your body");
+
+console.assert([...audio.getAttributeNames()].includes("controls"), "You must have an audio element with controls attribute");
+
+const audioContent = 'Your browser does not support HTML5 audio. Here is <a href="mp3s/YourAudioFileNameHere.mp3">a link to the audio</a>\n    instead.';
+
+console.assert(audio.innerHTML.includes(audioContent), `You must have an audio element with the following content: \n"${audioContent}" \nIf this message does not go away check your spelling and spacing or copy and paste from nucamp instructions`);
+// console.log(body.lastElementChild.previousElementSibling);
+////////////// don't forget to add point system//////////////// 
+// work in groups of 3-4 people
 // not possible to check for head and body because live server adds them automatically
-// document.addEventListener("DOMContentLoaded", checkFormat);
 // link for this test https://cdn.jsdelivr.net/gh/noobboon7/silver-spoon/src/workshop1.js
